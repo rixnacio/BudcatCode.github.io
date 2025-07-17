d707ed856c269d0757618abd2256473c14c72e45
BudcatCode <c.sgmoraga@gmail.com>
2025-07-17 17:18:19 -0400

    mi primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const typed = new Typed('.typed-text', {
+  strings: [
+    'Desarrolladora Front-End en formación',
+    'Diseñadora de Juegos Digitales',
+    'Diseñadora E-learning',
+  ],
+  typeSpeed: 60,
+  backSpeed: 30,
+  backDelay: 2000,
+  loop: true
+});
